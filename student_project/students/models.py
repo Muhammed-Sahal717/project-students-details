1084eff503ebe6fafba59c1e7141e12dab668ac7
muhammed sahal <muhammedsahalap717@gmail.com>
2026-09-01 21:33:46 +0530

    Add image field to Student model and update templates for displaying student images

--- student_project/students/models.py
+++ student_project/students/models.py
@@ -9,6 +9,7 @@ class Student(models.Model):
     email = models.EmailField()
     phone = models.CharField(max_length=15)
     address = models.TextField()
+    image = models.ImageField(upload_to='students/' , blank=True, null=True)
 
     def __str__(self):
         return self.name

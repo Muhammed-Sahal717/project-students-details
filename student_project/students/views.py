4f1cc24c9207690623e04f2770eb943ac516f175
muhammed sahal <muhammedsahalap717@gmail.com>
2026-09-01 20:12:59 +0530

    Add student detail view and associated styles

--- student_project/students/views.py
+++ student_project/students/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .models import Student
 
@@ -7,4 +7,11 @@ def student_list(request):
     students = Student.objects.all()
     return render(request, 'students/student_list.html', {
         'students': students
+    })
+
+def student_detail(request, id):
+    student = get_object_or_404(Student, id=id)
+
+    return render(request, 'students/student_detail.html', {
+        'student': student
     })
